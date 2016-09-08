7b40f4c45547f308aeccd75f8fbb66745aa0f478
Abigail Watson <awatson1978@gmail.com>
2016-09-07 22:07:14 -0500

    console logging at startup

--- webapp/server/main.js
+++ webapp/server/main.js
@@ -7,7 +7,14 @@ import '/imports/api/patients/methods';
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 
+
 Meteor.startup(function (){
+  if (Meteor.settings) {
+    console.log('Welcome to ' + Meteor.settings.title);
+  }
+  if (process.env.NODE_ENV) {
+    console.log('Launching environment: ' + process.env.NODE_ENV);    
+  }
 
   if (Meteor.users.find({username: 'janedoe'}).count() === 0) {
     let newAccount = Accounts.createUser({
