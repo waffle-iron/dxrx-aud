eb9df151e9a00999be88706eaf20baafd337dba1
Robert Nix <bob@dxrxmedical.com>
2016-09-16 16:55:58 -0700

    Build pipeline for Aptible and Testflight

---
+++ ios/ios/project/Alcohol Treatment/Plugins/cordova-plugin-bactrack/BacTrackPlugin.h
@@ -0,0 +1,22 @@
+#import <Foundation/Foundation.h>
+#import <Cordova/CDV.h>
+#import "BACtrack.h"
+#import "BACTrackPluginDelegate.h"
+
+@interface BACTrackPlugin : CDVPlugin
+
+-(void)connectToNearestBreathalyzer: (CDVInvokedUrlCommand*)command;
+-(void)connectBreathalyzer:(CDVInvokedUrlCommand*) command;
+-(void)connectBreathalyzerAsync:(CDVInvokedUrlCommand*) command;
+-(void)startScan:(CDVInvokedUrlCommand*) command;
+-(void)stopScan:(CDVInvokedUrlCommand*) command;
+-(void)disconnect:(CDVInvokedUrlCommand*) command;
+-(void)startCountdown:(CDVInvokedUrlCommand*) command;
+-(void)getBreathalyzerBatteryLevel:(CDVInvokedUrlCommand*) command;
+
+@property id delegateId;
+@property BACTrackPluginDelegate *delegate;
+@property BacTrackAPI *bacTrack;
+@property id bacTrackId;
+
+@end
