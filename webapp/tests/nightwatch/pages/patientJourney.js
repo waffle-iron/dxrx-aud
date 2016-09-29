e7ad30a72b5655468e4225b150504ae479a85174
Abigail Watson <awatson1978@gmail.com>
2016-09-28 20:15:31 -0500

    removed unnecessary test scripts

--- webapp/tests/nightwatch/pages/patientJourney.js
+++ webapp/tests/nightwatch/pages/patientJourney.js
@@ -1,37 +1,6 @@
 module.exports = {
   url: 'https://localhost:3000',
   commands: [{
-    login: function(email, password){
-      return this
-        .waitForElementPresent('#homePageLoggedOut', 10000)
-        .verify.elementPresent('#login')
-
-        .clearValue('#a0-signin_easy_email')
-        .clearValue('#a0-signin_easy_password')
-
-        .setValue('#a0-signin_easy_email', email)
-        .setValue('#a0-signin_easy_password', password)
-
-        .waitForElementPresent('#loginButton', 3000)
-        .click('#loginButton');
-    },
-    signup: function(user){
-      return this
-        .waitForElementPresent('#homePageLoggedOut', 3000)
-        .waitForElementPresent('#goRegister', 3000)
-        .click('#goRegister')
-        .waitForElementPresent('#entryTitle', 3000)
-        .verify.containsText('#entryTitle', 'REGISTER')
-
-        .clearValue('#a0-signup_easy_email')
-        .clearValue('#a0-signup_easy_password')
-
-        .setValue('#a0-signup_easy_email', user.email)
-        .setValue('#a0-signup_easy_password', user.password)
-
-        .waitForElementPresent('#registerButton', 3000)
-        .click('#registerButton');
-    },
     takeTour: function(){
       return this;
     },
